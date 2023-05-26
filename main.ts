413900a6691b25e36b39362dd4610a33fac70ce5
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-26 17:34:43 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,3 +8,7 @@ radio.onReceivedValue(function (name, value) {
     basic.showString("" + (input.temperature()))
 })
 radio.setGroup(105)
+led.plotBarGraph(
+radio.receivedPacket(RadioPacketProperty.SignalStrength),
+128
+)
